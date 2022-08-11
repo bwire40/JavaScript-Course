b6469e60e03003f63fa3abdba3a170fbe3d735b9
bwire40 <emmanuelbwire80@gmail.com>
2022-08-11 12:24:54 +0300

    #New Changes

--- Introduction/functions.js
+++ Introduction/functions.js
@@ -1,23 +1,45 @@
+"use strict";
 /**Function
  * declared by keyword function
  * executed when called/invopked
  * code can be reused using functions
  */
-function myfunction() {
-  console.log("This is a sample function");
-}
+// function myfunction() {
+//   console.log("This is a sample function");
+// }
 // myfunction();
 // functions can contain parameters inside the parenthesis
 // the values parsed inside the parenthesis are agruments
 
-function areaSquare(s1, s2) {
-  return s1 * s2; //js stops executing when it reaches the return statement
-}
-area = areaSquare(20, 20);
-// console.log(area);
+// function areaSquare(s1, s2) {
+//   return s1 * s2; //js stops executing when it reaches the return statement
+// }
+// let area = areaSquare(20, 20);
+// // console.log(area);
 
-function toCelsius(fahrenheit) {
-  return (5 / 9) * (fahrenheit - 32);
-}
-celsius = toCelsius(200);
-console.log(celsius);
+// function toCelsius(fahrenheit) {
+//   return (5 / 9) * (fahrenheit - 32);
+// }
+// const celsius = toCelsius(200);
+// console.log(celsius);
+
+// funtion expressions
+// these are functions that can be stored in variables
+// let x = function (a, b) {
+//   return a * b;
+// };
+// // the variable can b used as a function
+// const z = x(1, 3);
+// console.log(z);
+// x is an anonymous function, a function witout a name
+
+// function hoisting, js functions can be called before they are declared
+
+// self invoking functions
+// function expressions can be made self invoking
+(function () {
+  let x = "Hello, I called myself!";
+  console.log(x);
+})();
+// check the above syntax properly.
+// an example of a self invoking anonymous function
--- Introduction/objects.js
+++ Introduction/objects.js
@@ -1,10 +1,11 @@
+"use strict";
 /**Objects
  * these are containers for named values called properties or methods
  */
 // example of an object
-person = {
+const person = {
   firstname: "Emmanuel",
-  lastname: "Bwire",
+  lastname: "",
   eyeColor: "blue",
   age: function (currentYear, birthYear) {
     return currentYear - birthYear;
@@ -12,6 +13,12 @@ person = {
   details: function () {
     return `${this.firstname} ${this.lastname}. Eye Color: ${this.eyeColor}.
     Age: ${this.age(2022, 1999)}.`;
+  },
+  get eye_Color() {
+    return this.eyeColor;
+  },
+  set last_name(x) {
+    this.lastname = x;
   },
 };
 //this keyword refers to the owner of the function. person object owns the function age
@@ -38,3 +45,48 @@ person = {
 // console.log(person.bestFriend); //undefined
 
 // object methods
+// getter method using get keyword and setter method usign set keyword
+console.log(person.eye_Color);
+person.last_name = "Bwire";
+console.log(person.lastname);
+
+// Js can secure better data quality when using getters and setters
+// Object.defineProperty() can also be used to add getters and setters
+
+// OBJECT CONSTRUCTORS
+// constructor function
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.P_age = age;
+  this.eyeColor = eye;
+}
+// calliing the constructor functions
+const person1 = new Person("Emmanuel", "Bwire", 24, "Blue");
+
+Person.prototype.fullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+// console.log(Person);
+// adding a method to the object
+// person1.fullName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// };
+// console.log(person1.firstName);
+// console.log(person1.fullName());
+
+// to add new properties and methods to an existing object constructro use the prototype property.
+
+// ES5 OBJECT METHODS
+// change a property using Object.defineProperty(object,prperty,descriptor)
+Object.defineProperty(person, "firstname", { value: "Antony" });
+// adding a property
+Object.defineProperty(person, "language", { value: "Eng" });
+//adding getters and setters
+Object.defineProperty(person, "fullSent", {
+  get: function () {
+    return `${this.firstname} ${this.lastname} has ${this.eyeColor} eyes.`;
+  },
+});
+
+console.log(person.fullSent);
