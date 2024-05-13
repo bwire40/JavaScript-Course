2bfc060bc8b0ab65abf4aa5b1fbe354a98d3c769
bwire40 <emmanuelbwire80@gmail.com>
2024-05-13 10:49:00 +0300

    reference types

--- refresher/script.js
+++ refresher/script.js
@@ -11,23 +11,65 @@ comments
 
 // var let and const
 
-let firstName = "john";
-let lastName = "bwire";
-let age = 24;
+// let firstName = "john";
+// let lastName = "bwire";
+// let age = 24;
 
-let a, b, c;
+// let a, b, c;
 
-a = 20;
-b = 30;
-c = 40;
+// a = 20;
+// b = 30;
+// c = 40;
 
-console.log(c);
+// console.log(c);
 
-const arr = [1, 2, 3, 4];
+// const arr = [1, 2, 3, 4];
 
-arr.push(5);
-console.log(firstName, lastName, age);
-console.log(arr);
-console.log("***************************************");
+// arr.push(5);
+// console.log(firstName, lastName, age);
+// console.log(arr);
+// console.log("***************************************");
 
 //  data types
+
+/**
+ * primitive data types - predefined by the language and is named by a reserved keyword
+ *  string-sequence of characters,
+ *  number,
+ *  boolean-true or flase,
+ *  null-intentional empty value
+ *  undefined-undefined variable
+ *  symbol- built in object
+ *  BigInt-numbers that are greater than the number type
+ *
+ * reference types - or objects. non primitive, objects, arrays etc
+ * static typing vs dynamic typing
+ *      js is dynamically-types language. we dont explicitly define the types of our variables.
+ */
+
+const firstName = "sam";
+const age = 20;
+const temp = 29.6;
+const hasKids = true;
+const aptNum = null; // null shows its an object when getting its type is an object. but null is not an object.
+let score; //undefined variable returns undefined type
+
+const id = Symbol("íd"); //returns a unique sysmbol value
+
+// reference types
+
+// arrays,object literals, functions
+
+const arr = [1, 2, 3, 4, 5]; //array of numbers
+
+const person = {
+  name: "emmanuel",
+}; //object person
+
+const func = () => {
+  console.log("Hello");
+};
+
+const output = func; //function object
+
+console.log(output, typeof output);
