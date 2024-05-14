8fb097d7183a2fe9978099e2ce86fc11687b104f
bwire40 <emmanuelbwire80@gmail.com>
2024-05-14 10:03:30 +0300

    Working with math object

---
+++ refresher/6. Math Object/script.js
@@ -0,0 +1,23 @@
+// Math Object
+let x;
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(3.567); //rounds to the nearest integer
+
+x = Math.ceil(4.1); //Returns the smallest integer greater than or equal to its numeric argument
+
+x = Math.floor(3.9); //round down
+
+x = Math.pow(2, 8);
+
+x = Math.min(3, 2, 1);
+
+x = Math.max(2, 34, 5, 7);
+
+// creating a random number
+x = Math.random();
+x = Math.floor(Math.random() * 10 + 1);
+
+console.log(x);
